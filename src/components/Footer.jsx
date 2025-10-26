b9e1238fb7521251e7153c195211ccaed3cc7024
mustansar3755 <mustansar3755hussain@gmail.com>
2025-10-26 09:47:56 +0500

    Footer Section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import assets from "../assets/assets";
+import { links } from "../assets/data";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className=" bg-[#E8F5E9]">
+      <div className="top px-20 py-10 flex items-center justify-between">
+        <div className="text-2xl flex gap-4">
+          <img src={assets.Logo} alt="" />
+          Nextcent
+        </div>
+        <div className="inner">
+          <ul className=" text-2xl font-bold m-2 mt-6">
+            Nextcent
+            {links.map((link, index) => (
+              <li className=" text-lg font-normal" key={index}>{link.title}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <hr className=" text-textPrimary pt-10" />
+      <div className=" flex justify-between px-20 pb-10">
+        <h2>Term & Conditions | Privacy Policy</h2>
+        <h2>©2025 Nextcent. All Rights Reserved</h2>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
